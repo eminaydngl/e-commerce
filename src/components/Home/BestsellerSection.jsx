00158316f77bd0b0fb4668e6392dbfff038bab68
eminaydngl <emintr102006@gmail.com>
2026-08-29 20:35:04 +0300

    feat: homePage ile ilgili küçük düzeltmeler yapıldı

--- src/components/Home/BestsellerSection.jsx
+++ src/components/Home/BestsellerSection.jsx
@@ -11,17 +11,17 @@ function BestsellerSection(){
             </div>
 
             <div className="flex flex-col items-center md:flex-row gap-8 justify-center">
-                <ProductCard picture={"https://picsum.photos/id/614/400/500"}/>
-                <ProductCard picture={"https://picsum.photos/id/643/400/500"}/>
-                <ProductCard picture={"https://picsum.photos/id/604/400/500"}/>
-                <ProductCard picture={"https://picsum.photos/id/424/400/500"}/>
+                <ProductCard id={614} picture={"https://picsum.photos/id/614/400/500"}/>
+                <ProductCard id={643} picture={"https://picsum.photos/id/643/400/500"}/>
+                <ProductCard id={604} picture={"https://picsum.photos/id/604/400/500"}/>
+                <ProductCard id={424} picture={"https://picsum.photos/id/424/400/500"}/>
             </div>
 
             <div className="flex flex-col items-center md:flex-row gap-8 justify-center">
-                <ProductCard picture={"https://picsum.photos/id/124/400/500"}/>
-                <ProductCard picture={"https://picsum.photos/id/1000/400/500"}/>
-                <ProductCard picture={"https://picsum.photos/id/144/400/500"}/>
-                <ProductCard picture={"https://picsum.photos/id/111/400/500"}/>
+                <ProductCard id={124} picture={"https://picsum.photos/id/124/400/500"}/>
+                <ProductCard id={1000} picture={"https://picsum.photos/id/1000/400/500"}/>
+                <ProductCard id={144} picture={"https://picsum.photos/id/144/400/500"}/>
+                <ProductCard id={111} picture={"https://picsum.photos/id/111/400/500"}/>
 
             </div>
         </section>
